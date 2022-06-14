8a74dca83a6862d79e90229646f2bce78c735951
silverwind <me@silverwind.io>
2022-06-14 21:46:16 +0200

    Really fix empty line rendering
    Followup to #19798. Trim off both the .line and .cl classes from
    Chroma's HTML which made the old conditional work again. This fixed Copy
    of YAML files for me.

--- modules/highlight/highlight.go
+++ modules/highlight/highlight.go
@@ -198,16 +198,18 @@ func File(numLines int, fileName, language string, code []byte) []string {
 	m := make([]string, 0, numLines)
 	for _, v := range strings.SplitN(htmlbuf.String(), "\n", numLines) {
 		content := string(v)
+
+		// remove useless wrapper nodes
+		content = strings.Replace(content, "<span class=\"line\"><span class=\"cl\">", "", 1)
+		content = strings.TrimPrefix(content, `</span></span>`)
+
 		// need to keep lines that are only \n so copy/paste works properly in browser
 		if content == "" {
 			content = "\n"
 		} else if content == `</span><span class="w">` {
-			content += "\n</span>"
-		} else if content == `</span></span><span class="line"><span class="cl">` {
 			content += "\n"
 		}
-		content = strings.TrimSuffix(content, `<span class="w">`)
-		content = strings.TrimPrefix(content, `</span>`)
+
 		m = append(m, content)
 	}
 	if finalNewLine {
